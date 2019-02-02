e8b29e8dc1ee68a9de92e3daa1a9c2237827009f
Vlad V <vladv..@macbook-pro-vlad.local>
2019-02-02 15:07:04 +0300

    Initial Commit

---
+++ GuessWord/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GuessWord
+//
+//  Created by Vlad V.. on 02/02/2019.
+//  Copyright © 2019 Vlad V. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
