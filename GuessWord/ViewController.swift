e8c2a6266ab22959eededf19e703e222a336a55a
Vlad V <vladv..@macbook-pro-vlad.local>
2019-02-02 16:14:29 +0300

    1

--- GuessWord/ViewController.swift
+++ GuessWord/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var topStackView: UIStackView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        
+        if size.width < size.height{
+            topStackView.axis = .vertical
+        }
+        else{
+            topStackView.axis = .horizontal
+        }
+    }
 }
 
